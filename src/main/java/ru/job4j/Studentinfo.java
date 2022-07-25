9c3203f2defde701b37e35776d8943d6c41c4421
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-07-25 14:10:12 +0900

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Studentinfo {
     public static void main(String[] args) {
-        System.out.println("Petr Arsentev");
+        System.out.println("прив");
         System.out.println("06.01.2019");
     }
 
