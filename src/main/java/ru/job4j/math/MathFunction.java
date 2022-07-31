2e0f47f30298ef2bed36105cd57135357be25060
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-01 01:20:39 +0900

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -1,24 +1,15 @@
 ﻿package ru.job4j.math;
-import static ru.job4j.math.MathFunction.*;
+
 public class MathFunction {
     public static double sum(double first, double second) {
-
         return first + second;
-    }
-    public static double min(double first, double second) {
+    }    public static double multiply(double first, double second) {
 
-        return first - second;
-    }
-    public static double del(double first, double second) {
-
-        return first / second;
-    }
-    public static double multiply(double first, double second) {
         return first * second;
     }
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
+         return ru.job4j.math.MathFunction.sum(first, second)
+                + ru.job4j.math.MathFunction.multiply(first, second);
     }
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
