5d2f48ed925c7c173eca2594f4144e9fe5a4dc73
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-11 20:08:59 +0900

    1.1. Что такое import [#504773 #327377]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,20 +1,21 @@
 package ru.job4j.calculator;
 
-import ru.job4j.math.MathFunction;
+import static ru.job4j.math.MathFunction.*;
+
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second);
+        return sum(first, second)
+                + multiply(first, second);
     }
 
     public static double delAndMinus(double first, double second) {
-        return MathFunction.del(first, second)
-                + MathFunction.minus(first, second);
+        return del(first, second)
+                + minus(first, second);
     }
 
     public static double sumAndMultiplyAndminusAnddel(double first, double second) {
-        return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second) + MathFunction.minus(first, second) + MathFunction.del(first, second);
+        return sum(first, second)
+                + multiply(first, second) + minus(first, second) + del(first, second);
     }
 
     public static void main(String[] args) {
