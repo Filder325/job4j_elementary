2a613c96e0a21ec41580958af4ba4dea33926c02
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-11 10:42:21 +0900

    1.1. Что такое import [#504773 #327377]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -7,4 +7,10 @@ public class MathFunction {
     public static double multiply(double first, double second) {
         return first * second;
     }
+    public static double del(double first, double second) {
+        return first / second;
+    }
+    public static double minus(double first, double second) {
+        return first - second;
+    }
 }
