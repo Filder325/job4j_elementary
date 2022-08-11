1a5a9d17be1be0842dcd70d2f13e22687ef46bf4
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-11 11:14:28 +0900

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -6,14 +6,17 @@ public class MathCalculator {
         return MathFunction.sum(first, second)
                 + MathFunction.multiply(first, second);
     }
+
     public static double delAndMinus(double first, double second) {
         return MathFunction.del(first, second)
                 + MathFunction.minus(first, second);
     }
+
     public static double sumAndMultiplyAndminusAnddel(double first, double second) {
         return MathFunction.sum(first, second)
-                + MathFunction.multiply(first, second) + MathFunction.minus(first, second) + MathFunction.del(first, second) ;
+                + MathFunction.multiply(first, second) + MathFunction.minus(first, second) + MathFunction.del(first, second);
     }
+
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета равен: " + delAndMinus(10, 20));
