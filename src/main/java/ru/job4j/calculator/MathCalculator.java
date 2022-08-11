91e4f2bfad4942bb46d542c47ea24aacb2628519
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-11 20:24:29 +0900

    1.1. Что такое import [#504773 #327377]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -13,7 +13,7 @@ public class MathCalculator {
                 + minus(first, second);
     }
 
-    public static double sumAndMultiplyAndminusAnddel(double first, double second) {
+    public static double sumAndMultiplyAndMinusAndDel(double first, double second) {
         return sum(first, second)
                 + multiply(first, second) + minus(first, second) + del(first, second);
     }
@@ -21,6 +21,6 @@ public class MathCalculator {
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета равен: " + delAndMinus(10, 20));
-        System.out.println("Результат расчета равен: " + sumAndMultiplyAndminusAnddel(10, 20));
+        System.out.println("Результат расчета равен: " + sumAndMultiplyAndMinusAndDel(10, 20));
     }
 }
