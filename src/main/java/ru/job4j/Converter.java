7f2ea9bf80fa3c8ba5ac77acca2e42ccd887e937
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-13 01:19:09 +0900

    1.1. Что такое import [#504773 #327377]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -14,7 +14,7 @@ public class Converter {
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are1 " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
     }
 }
