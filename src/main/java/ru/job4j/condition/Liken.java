14941ee6ffbe7a0fda7fdd1e24a222c4487b54af
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-15 01:30:41 +0900

    0. Операции сравнения [#235631 #331743]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Liken {
     public static void main(String[] args) {
-    int first = 110;
+    int first = 10;
     int second = 9;
     boolean result = first > second;
     System.out.println(result);
