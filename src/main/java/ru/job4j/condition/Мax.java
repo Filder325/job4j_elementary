07ff08d73274e91fb90d4993f195d1e04d3a70a6
Filder325 <109712856+Filder325@users.noreply.github.com>
2022-08-15 22:37:52 +0900

    4.2. Тернарное сравнение [#189 #332249]

--- src/main/java/ru/job4j/condition/Мax.java
+++ src/main/java/ru/job4j/condition/Мax.java
@@ -1,4 +1,8 @@
 package ru.job4j.condition;
 
 public class Мax {
+    public static int max(int left, int right) {
+        int result = left >= right ? left: right;
+        return result;
+    }
 }
